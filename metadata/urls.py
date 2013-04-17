8d57093f41ddf54c319fad080ad96c93d251e5dd
rastermanden <martin@septima.dk>
2013-04-17 23:26:30 +0200

    edited readme

--- metadata/urls.py
+++ metadata/urls.py
@@ -1,9 +1,10 @@
 # urls.py
 from django.conf.urls import patterns, url
 from metadata.views import MetadataList, MetadataDetail
+from django.http import HttpResponseRedirect
 
 urlpatterns = patterns('',
-    url(r'^$', lambda x: HttpResponseRedirect('/list/')),
+    url(r'^$', lambda x: HttpResponseRedirect('/list/')), ##http://matthowell.com/blog/2009/12/12/django-url-redirect-shortcut/
     url(r'^list/', MetadataList.as_view(), name='list'),
     url(r'^detail/(?P<slug>[-_\w]+)/$', MetadataDetail.as_view(), name='detail'),
 
