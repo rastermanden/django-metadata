b094c0a16f19d4ebad9f0edaef2d8877ceb9b648
rastermanden <martin@septima.dk>
2013-04-17 22:51:38 +0200

    edited readme

--- metadata/urls.py
+++ metadata/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from metadata.views import MetadataList, MetadataDetail
 
 urlpatterns = patterns('',
-    url(r'^list/', MetadataList.as_view(), name='list'),
+    url(r'^', MetadataList.as_view(), name='list'),
     url(r'^detail/(?P<slug>[-_\w]+)/$', MetadataDetail.as_view(), name='detail'),
 
 )
