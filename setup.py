19d9c15f2a0ff5626d0d7b8601712e0c44135710
rastermanden <martin@septima.dk>
2013-04-17 20:52:31 +0200

    edited readme

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 from setuptools import setup
-
+from setuptools import find_packages
 README = open(os.path.join(os.path.dirname(__file__), 'README.txt')).read()
 
 # allow setup.py to be run from any path
