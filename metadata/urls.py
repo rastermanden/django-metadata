feeab01a0193609781c8b10f180f1177abaad8e6
rastermanden <martin@septima.dk>
2013-04-17 23:43:13 +0200

    edited readme

--- metadata/urls.py
+++ metadata/urls.py
@@ -7,7 +7,7 @@ from django.views.generic.base import RedirectView
 urlpatterns = patterns('',
    # url(r'^$', lambda x: HttpResponseRedirect('/list/')), ##http://matthowell.com/blog/2009/12/12/django-url-redirect-shortcut/
    # url(r'^$', redirect_to, {'url': '/list'}),
-   url(r'^g$', RedirectView.as_view(url='/list'), name='go-to-django'),
+   url(r'^$', RedirectView.as_view(url='/list'), name='go-to-django'),
 
    url(r'^list/', MetadataList.as_view(), name='list'),
     url(r'^detail/(?P<slug>[-_\w]+)/$', MetadataDetail.as_view(), name='detail'),
